bf364473b5cd1b75ba3b72d474a74e04204eb5a1
Anna Rose Thomas <147629016+anna-rose-004@users.noreply.github.com>
2025-07-22 20:07:14 +0530

    Create postorder.py

---
+++ Tree/postorder.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        def order(root):
+            if not root:
+                return []
+            return order(root.left)+order(root.right)+[root.val]
+        return order(root)

b975fe1845afc0fa0ff7638b5386cd56797f45c9
Anna Rose Thomas <147629016+anna-rose-004@users.noreply.github.com>
2025-07-22 21:59:03 +0530

    Create levelorder.py

---
+++ Tree/levelorder.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        q=deque([root])
+        result=[]
+        while q:
+            size=len(q)
+            curr_level=[]
+            for _ in range(size):
+                node=q.popleft()
+                curr_level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            result.append(curr_level)
+        return result
